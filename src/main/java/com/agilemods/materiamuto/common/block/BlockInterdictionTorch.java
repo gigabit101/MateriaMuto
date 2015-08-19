59fdb16fab1ffe8808cc48bcfbe1c6594ec5d880
gigabit101 <thegigabit101@gmail.com>
2015-08-19 17:15:12 +0100

    Finished off the Interdiction Torch

--- src/main/java/com/agilemods/materiamuto/common/block/BlockInterdictionTorch.java
+++ src/main/java/com/agilemods/materiamuto/common/block/BlockInterdictionTorch.java
@@ -3,6 +3,7 @@ package com.agilemods.materiamuto.common.block;
 import com.agilemods.materiamuto.common.tile.TileInterdictionTorch;
 import net.minecraft.block.BlockTorch;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -11,5 +12,10 @@ public class BlockInterdictionTorch extends BlockTorch implements ITileEntityPro
     @Override
     public TileEntity createNewTileEntity(World world, int meta) {
         return new TileInterdictionTorch();
+    }
+    
+    @Override
+    public void registerBlockIcons(IIconRegister icon) {
+    	blockIcon = icon.registerIcon("materiamuto:interdiction_torch"); 
     }
 }
